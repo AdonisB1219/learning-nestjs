d78b83dfc0a266daa8cde0fb48d4c76137a4c0df
Adonis Becerra Morales <89605756+AdonisB1219@users.noreply.github.com>
2023-11-27 12:41:55 -0600

    Update events.controller.ts

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -7,7 +7,56 @@ import { Event } from './event.entity'
 export class EventsController{
 
     private events: Event[] = [];
+import { Controller, Get, Post, Patch, Delete, Param, Body, HttpCode } from "@nestjs/common";
+import { CreateEventDto } from './create-event.dto'
+import { UpdateEventDto } from "./update-event.dto";
+import { Event } from './event.entity'
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
+@Controller('/events')
+export class EventsController{
+    constructor(
+        @InjectRepository(Event)
+        private readonly repository: Repository<Event>
+    ){}
+
+    @Get()
+    async findAll(){
+        return await this.repository.find();
+    }
+    @Get(':id')
+    async findOne(@Param('id') id){
+        return await this.repository.findOne(id);;
+    }
+    
+    @Post()
+    async create(@Body() input: CreateEventDto){
+        return await this.repository.save({
+            ...input,
+            when: new Date(input.when),
+        });
+    }
+
+    @Patch(':id')
+    async update(@Param('id') id, @Body() input: UpdateEventDto){
+        var entity = await this.repository.findOne(id);
+        entity = {
+            ...entity,
+            ...input,
+            when: input.when ? new Date(input.when) : entity.when
+        }
+        return await this.repository.save(entity);
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    async remove(@Param('id') id){
+        const event = await this.repository.findOne(id);
+        await this.repository.remove(event);
+        
+    }
+}
     @Get()
     findAll(){
         return [
